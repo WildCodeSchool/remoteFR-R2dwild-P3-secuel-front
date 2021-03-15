3c2ec02f9b0bd8235f65f271285f2f7c1bbdb522
xenogearsX <xenogears@hotmail.fr>
2021-03-15 11:48:31 +0100

    put event component in home screen

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,5 +1,11 @@
+import Events from '../components/Events'
+import './Home.css'
 const Home = () => {
-  return <div className='home'></div>
+  return (
+    <div className='home'>
+      <Events />
+    </div>
+  )
 }
 
 export default Home
