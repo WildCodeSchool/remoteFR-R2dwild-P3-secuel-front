f77be6b306b665d98986552d8f8fdc23d5680f5a
xenogearsX <xenogears@hotmail.fr>
2021-03-15 16:56:20 +0100

    change detail event to filter the fake data and match the id

--- src/components/DetailEvent.js
+++ src/components/DetailEvent.js
@@ -1,33 +1,33 @@
-const DetailEvent = insured => {
+import acte from '../data/fakejson/fakedata.json'
+
+const DetailEvent = test => {
+  const insured = acte.filter(
+    medevent => medevent.id === parseInt(test.match.params.id)
+  )[0]
   return (
-    <div className='events'>
-      <div
-        className='card'
-        key={insured.Nom + insured.date + insured['Type acte']}
-      >
-        <p className='titreCard'>{insured['N Sécurité sociale']}</p>
-        <p>
-          <span>Date acte :</span> {insured['date acte médicale']}
-        </p>
-        <p>
-          <span>Assuré :</span> {insured.Nom} {insured['prénom']}
-        </p>
-        <p>
-          <span>Acte :</span> {insured['Type acte']}
-        </p>
-        <p>
-          <span>Spécialiste : </span>
-          {insured['Nom professionnel de santé']}
-        </p>
-        <p>
-          <span>Montant payé :</span>
-          {insured['Montant Payé']}
-        </p>
-        <p>
-          <span>Montant remboursé :</span>
-          {insured['Montant remboursé'] ? insured['Montant remboursé'] : 0}
-        </p>
-      </div>
+    <div className='card'>
+      <p className='titreCard'>{insured['N Sécurité sociale']}</p>
+      <p>
+        <span>Date acte: </span> {insured['date acte médicale']}
+      </p>
+      <p>
+        <span>Assuré: </span> {insured.Nom} {insured['prénom']}
+      </p>
+      <p>
+        <span>Acte: </span> {insured['Type acte']}
+      </p>
+      <p>
+        <span>Spécialiste: </span>
+        {insured['Nom professionnel de santé']}
+      </p>
+      <p>
+        <span>Montant payé: </span>
+        {insured['Montant Payé']}
+      </p>
+      <p>
+        <span>Montant remboursé: </span>
+        {insured['Montant remboursé'] ? insured['Montant remboursé'] : 0}
+      </p>
     </div>
   )
 }
