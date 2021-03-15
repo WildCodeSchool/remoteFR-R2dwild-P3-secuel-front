0fe6e831dd9f085fa32046658793507b42d1d0d0
xenogearsX <xenogears@hotmail.fr>
2021-03-15 16:54:09 +0100

    add route to detailevent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Analyse from './screen/Analyse'
 import CreateUser from './screen/CreateUser'
+import DetailEvent from './components/DetailEvent'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './screen/Home'
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/notification' component={Notification} />
         <Route path='/analyse' component={Analyse} />
         <Route path='/params' component={Params} />
+        <Route path='/event/:id' component={DetailEvent} />
       </Switch>
       <Footer />
     </div>
