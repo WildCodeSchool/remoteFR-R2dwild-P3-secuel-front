e215551e8c36da10eefa62c86e76d6047c75ec86
xenogearsX <xenogears@hotmail.fr>
2021-03-15 11:49:53 +0100

    create simple component to match each route

---
+++ src/screen/Params.js
@@ -0,0 +1,19 @@
+const Params = () => {
+  return (
+    <div>
+      <p>Choisissez l&apos;affichage de vos comptes</p>
+      <ul>
+        <li>Tous les assurés</li>
+        <li>Assurés principale</li>
+      </ul>
+      <p>Supprimez un compte</p>
+      <select>
+        <option>Jean Dupont</option>
+        <option>Lucas Dupont</option>
+        <option>Marie Dupont</option>
+        <option>Léa Dupont</option>
+      </select>
+    </div>
+  )
+}
+export default Params
