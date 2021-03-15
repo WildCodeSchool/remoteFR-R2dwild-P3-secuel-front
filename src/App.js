f8ef02d670c0ff4c6053e361eb1bf4f193b27b03
xenogearsX <xenogears@hotmail.fr>
2021-03-15 11:47:09 +0100

    delete app.test and add routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react'
-import Home from './screen/Home'
-
-import { Switch, Route } from 'react-router-dom'
-
-import './App.css'
-
-import Events from './components/Events'
+import Analyse from './screen/Analyse'
+import CreateUser from './screen/CreateUser'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import CreateUser from './screen/CreateUser'
+import Home from './screen/Home'
+import Message from './screen/Message'
+import Notification from './screen/Notification'
+import Params from './screen/Params'
+
+import { Switch, Route, Link } from 'react-router-dom'
+
+import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
-      <Home />
-      <Events />
-      <Footer />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/CreateUser' component={CreateUser} />
+        <Route path='/message' component={Message} />
+        <Route path='/notification' component={Notification} />
+        <Route path='/analyse' component={Analyse} />
+        <Route path='/params' component={Params} />
       </Switch>
+      <Link to='/message' className='bouton'>
+        Messages
+      </Link>
+      <Link to='/notification' className='bouton'>
+        Notifications
+      </Link>
+      <Link to='/analyse' className='bouton'>
+        Analyse
+      </Link>
+      <Link to='/params' className='bouton'>
+        Paramètres
+      </Link>
+      <Footer />
     </div>
   )
 }
