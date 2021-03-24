a672085c6275a08e5905ebfc5eeab73124aab662
Christellefz <christellefz@yahoo.fr>
2021-03-24 10:13:10 +0100

    admin form notif et spe

---
+++ src/screen/FormAdminSecialities.js
@@ -0,0 +1,62 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+import './Form.css'
+
+const FormAdminSpeciality = () => {
+  const [speciality, setSpeciality] = useState('')
+
+  const spePost = {
+    specialiti_name: speciality
+  }
+
+  const handleChange = e => {
+    e.target.name === 'speciality' ? setSpeciality(e.target.value) : null
+  }
+
+  const submitForm = e => {
+    e.preventDefault()
+    axios
+      .post('http://localhost:3000/notifications', spePost)
+      .then(res => {
+        alert(`${res.data} !`)
+      })
+      .catch(e => {
+        console.error(e)
+        alert(`Erreur lors de la création : ${e.message}`)
+      })
+  }
+
+  return (
+    <div className='form'>
+      <h1>Création de compte</h1>
+      <form onSubmit={submitForm}>
+        <fieldset>
+          <legend>Informations</legend>
+          <div className='form-data'>
+            <label htmlFor='speciality'>
+              Type de message<span> * </span>
+            </label>
+            <input
+              type='text'
+              id='speciality'
+              name='speciality'
+              onChange={handleChange}
+              required
+              value={speciality}
+            />
+          </div>
+          <hr />
+          <p>
+            <span> * </span> required.
+          </p>
+          <div className='form-data'>
+            <input type='submit' value='Envoyer' />
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default FormAdminSpeciality
