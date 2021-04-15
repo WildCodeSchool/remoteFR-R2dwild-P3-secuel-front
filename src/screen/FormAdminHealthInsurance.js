909ea3ed8e2864a8a0cd01632933eeff545206ac
MarcLall <marclallau@gmail.com>
2021-04-15 17:06:35 +0200

    merge incompreantion

--- src/screen/FormAdminHealthInsurance.js
+++ src/screen/FormAdminHealthInsurance.js
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import axios from 'axios'
-
 import './Form.css'
+
+import axios from 'axios'
+import { useState } from 'react'
 
 const FormAdminHealthInsurance = () => {
   const [message, setMessage] = useState(null)
--- src/screen/FormAdminPros.js
+++ src/screen/FormAdminPros.js
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import axios from 'axios'
-
 import './Form.css'
+
+import axios from 'axios'
+import { useState } from 'react'
 
 const FormAdminPros = () => {
   const [message, setMessage] = useState(null)
