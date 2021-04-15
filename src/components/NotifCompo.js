3d10a9aa08e5d9d87bfaca8cc5d1367b1d3d6e19
xenogearsX <xenogears@hotmail.fr>
2021-04-15 19:48:11 +0200

    debut notif

--- src/components/NotifCompo.js
+++ src/components/NotifCompo.js
@@ -4,17 +4,28 @@ const NotifCompo = notif => {
   return (
     <div
       className={
-        notif.status[0] === 'Traité' && notif.status[1]
+        notif.status[0] === 'Traité' && notif.status[1] === 'Traité'
           ? 'barGreen'
-          : notif.status[0] !== 'En cours de traitement' ||
-            notif.status[1] !== 'En cours de traitement'
-          ? 'barRed'
-          : 'barYellow'
+          : (notif.status[0] === 'En cours de traitement' &&
+              notif.payer === 'Sécurité Sociale') ||
+            (notif.status[1] === 'En cours de traitement' &&
+              notif.payer !== 'Sécurité Sociale')
+          ? 'barYellow'
+          : 'barRed'
       }
     >
       <p>{notif.notifDate}</p>
       <p>
-        <strong>{notif.firstName} </strong>a obtenu un remboursement de
+        <strong>{notif.firstName} </strong>
+        {notif.status[0] === 'Traité' && notif.status[1] === 'Traité'
+          ? 'a obtenu un remboursement de'
+          : (notif.status[0] === 'En cours de traitement' &&
+              notif.payer === 'Sécurité Sociale') ||
+            (notif.status[1] === 'En cours de traitement' &&
+              notif.payer !== 'Sécurité Sociale')
+          ? 'attend le remboursement de'
+          : 'a un problème de remboursement de'}
+        {/* notif.status[1] !== 'En cours de traitement'? ''} */}
         <strong> {notif.payer} </strong>
         pour la dépense de
         <strong> {notif.acteType} </strong>avec
